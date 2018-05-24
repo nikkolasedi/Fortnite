f62f41ca613158d5ddb8323c6520bfeebdeb3133
Nikkolas Edi P <nikkolasep@gmail.com>
2018-05-25 01:57:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/nikkolasedip/fortnite/ScrollingActivity3.java
@@ -0,0 +1,46 @@
+package com.example.nikkolasedip.fortnite;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class ScrollingActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling3);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        ImageButton imageButton1 = (ImageButton) findViewById(R.id.imageButton1);
+        imageButton1.setOnClickListener(new View.OnClickListener() {
+                                       public void onClick(View v) {
+                                           openAnotherActivity("PopUpActivity");
+                                       }
+                                   }
+        );
+    }
+    public void openAnotherActivity(String activityName){
+        try {
+            Class<?> activityClass = Class.forName("com.example.nikkolasedip.fortnite."+activityName);
+            Intent intent = new Intent(this, activityClass);
+            startActivity(intent);
+        } catch (ClassNotFoundException e ) {
+            e.printStackTrace();
+        }
+    }
+}
