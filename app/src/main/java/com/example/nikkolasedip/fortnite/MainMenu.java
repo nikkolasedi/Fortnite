3bb503605b068b463df9482143c9041367f78d82
GabrielShaukan <gabrielshaukan@yahoo.com>
2018-05-25 11:02:32 +0200

    Changed Scrolling activity name to MainMenu and renamed the buttons and their IDs

--- app/src/main/java/com/example/nikkolasedip/fortnite/MainMenu.java
+++ app/src/main/java/com/example/nikkolasedip/fortnite/MainMenu.java
@@ -10,14 +10,13 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.ImageButton;
 
-public class ScrollingActivity extends AppCompatActivity {
+public class MainMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scrolling);
+        setContentView(R.layout.activity_main_menu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
